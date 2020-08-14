7a90457d30647b95a9f00dad216730c0a33f4c5c
JohnNils Olson <johnnils@gmail.com>
2020-08-14 14:25:44 -0700

    fix automatic re-routing for unauthorized users due to naming conventions

--- PierresTreats/Controllers/AccountController.cs
+++ PierresTreats/Controllers/AccountController.cs
@@ -8,12 +8,12 @@ using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
-  public class AccountsController : Controller
+  public class AccountController : Controller
   {
     private readonly PierresTreatsContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
-    public AccountsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresTreatsContext db)
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresTreatsContext db)
     {
       _userManager = userManager;
       _signInManager = signInManager;
