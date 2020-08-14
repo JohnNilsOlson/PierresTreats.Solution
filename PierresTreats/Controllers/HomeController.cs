77ea00f2357c27d3117573edf32475c7f17179d6
JohnNils Olson <johnnils@gmail.com>
2020-08-14 10:01:47 -0700

    add home controller

---
+++ PierresTreats/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
+
+namespace PierresTreats.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
+      return View();
+    }
+  }
+}
