c9e55657bcfc74c1de5cc356120465b8d0ebf668
JohnNils Olson <johnnils@gmail.com>
2020-08-14 13:06:52 -0700

    add treats controller

---
+++ PierresTreats/Controllers/Treats.Controller.cs
@@ -0,0 +1,129 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatsController(PierresTreatsContext db, UserManager<ApplicationUser> userManager)
+    {
+      _db = db;
+      _userManager = userManager;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.Flavors)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [Authorize]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat)
+    {
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      )
+    }
+
+    [Authorize]
+    public ActionResult Edit(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      IQueryable<TreatFlavor> treatFlavors = _db.TreatFlavors.Where(entry => entry.TreatId == treat.TreatId);
+      foreach (TreatFlavor entry in treatFlavors)
+      {
+        if (entry.FlavorId == FlavorId)
+        {
+          return RedirectToAction("Details", new { id = treat.TreatId });
+        }
+        if (FlavorId != 0)
+        {
+          _db.TreatFlavors.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Details", new { id = treat.TreatId });
+      }
+    }
+
+    [Authorize]
+    public ActionResult DeleteFlavor(int id)
+    {
+      var joinEntry = _db.TreatFlavors.FirstOrDefault(entry => entry.TreatFlavorId == id);
+      ViewBag.Flavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == joinEntry.FlavorId);
+      ViewBag.Treat = _db.Treats.FirstOrDefault(treat => treat.TreatId == joinEntry.TreatId);
+      return View(joinEntry);
+    }
+
+    [HttpPost, ActionName("DeleteFlavor")]
+    public ActionResult DeleteFlavorConfirmed(int id)
+    {
+      var joinEntry = _db.TreatFlavors.FirstOrDefault(entry => entry.TreatFlavorId == id);
+      _db.TreatFlavors.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.TreatId });
+    }
+  }
+}
